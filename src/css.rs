7c7f5ccb3e90c8337fa01e4226fa3bed9c659d63
j4ck <50369519+j4cegh@users.noreply.github.com>
2022-05-16 22:47:41 +0300

    Fix css's foldering

--- src/css.rs
+++ src/css.rs
@@ -17,18 +17,16 @@ pub fn dist_css() {
             continue;
         }
 
-        if path.is_dir() {
-            let f_path_dist = format!(r"{}\dist\{}", dir.to_str().unwrap(), file_name);
-            println!("Log[NF]: {}", f_path_dist);
-            if !Path::new(&f_path_dist).exists() {
-                create_dir_all(&f_path_dist).unwrap();
-            }
-        }
-        else if path.is_file() {
+        if path.is_file() {
             let file_path = path.to_str().unwrap().split("src").collect::<Vec<&str>>()[1];
             let f_path_dist = format!(r"{}\dist\{}", dir.to_str().unwrap(), file_path);
+            let path_without_file = f_path_dist.split(file_name).collect::<Vec<&str>>()[0];
 
             if file_name.ends_with(".css") {
+                if !Path::new(&path_without_file).exists() {
+                    create_dir_all(&path_without_file).unwrap();
+                }
+
                 File::create(Path::new(&f_path_dist)).expect("Couldn't create dist file.");
 
                 let mut file = File::open(path).expect("Couldn't open file.");
