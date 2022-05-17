b34539f2d52b5442b3b89b17e3f5b1f912b3eead
j4ck <50369519+j4cegh@users.noreply.github.com>
2022-05-17 22:03:20 +0300

    Allow TS in body

--- src/html.rs
+++ src/html.rs
@@ -1,25 +1,34 @@
 use std::fs::File;
 use std::io::Read;
 use kuchiki;
+use kuchiki::{ElementData, NodeDataRef};
 use kuchiki::traits::TendrilSink;
 
-pub fn replace_ts(html: &str) -> String {
-    let doc = kuchiki::parse_html().one(html);
-    let head = doc.select_first("head").unwrap();
+fn do_replace(elem: NodeDataRef<ElementData>) {
+    let mut attrs = elem.attributes.borrow_mut();
+    let name = elem.name.local.to_string();
 
-    for elem in head.as_node().children().filter_map(|n| n.into_element_ref()) {
-        let mut attrs = elem.attributes.borrow_mut();
+    if name == "script" {
         if let Some(src) = attrs.get_mut("src") {
             if let Some(x) = src.strip_suffix(".ts") {
-                *src = format!("{x}.js");
+                *src = format!("{}.js", x);
             }
         }
 
-        let name = elem.name.local.to_string();
+        attrs.insert("type", "module".to_string());
+    }
+}
+pub fn replace_ts(html: &str) -> String {
+    let doc = kuchiki::parse_html().one(html);
+    let head = doc.select_first("head").unwrap();
+    let body = doc.select_first("body").unwrap();
 
-        if name == "script" {
-            attrs.insert("type", "module".to_string());
-        }
+    for elem in head.as_node().children().filter_map(|n| n.into_element_ref()) {
+        do_replace(elem);
+    }
+
+    for elem in body.as_node().children().filter_map(|n| n.into_element_ref()) {
+        do_replace(elem);
     }
 
     doc.to_string()
